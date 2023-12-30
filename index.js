b929c327d6045ae9f04db903d6dcaa15a60059c7
HarsikaKumari <kumariharsika8@gmail.com>
2023-12-30 21:22:18 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,61 @@
+// Making loop to get each button element
+for (var i = 0; i < document.querySelectorAll(".drum").length; i++){
+
+// Click function for detecting key board press
+document.querySelectorAll(".drum")[i].addEventListener("click",function () {
+    makeSound(this.innerHTML);
+});
+
+}
+// /Detecting keyboard press
+
+document.addEventListener("keypress", function(Event) {
+    makeSound(Event.key);
+});
+
+
+
+// Keypress function
+
+function makeSound(key) {
+
+    switch (key) {
+        case "w":
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        case "a":
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+        case "s":
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        case "d":
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+        case "j":
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+        case "k":
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+        case "l":
+            var kickBass = new Audio("sounds/kick-bass.mp3");
+            kickBass.play();
+            break;
+    
+        default: console.log(this.innerHTML);
+            break;
+    }    
+}
+
+
+
+
+
+
